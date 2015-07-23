7d3c079d2f4f08dc86583adaa52e2757d7aabe78
2zeez <2zeez@users.noreply.github.com>
2015-07-23 03:20:11 -0400

    Initial Commit

---
+++ FizzBuzzNezz/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FizzBuzzNezz
+//
+//  Created by Aziz Raouf on 2015-07-23.
+//  Copyright (c) 2015 Aziz Raouf. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
