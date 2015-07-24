44178909e2420f3078e3f20b6e1b95154ffef519
2zeez <2zeez@users.noreply.github.com>
2015-07-23 23:22:48 -0400

    Updated main with final work

--- FizzBuzzNezz/main.c
+++ FizzBuzzNezz/main.c
@@ -8,8 +8,28 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
+int main() {
     // insert code here...
-    printf("Hello, World!\n");
+    int x, y, z;
+    printf("Enter first integer multiple to replace with Fizz: ");
+    scanf("%d", &x);
+    printf("\nEnter second integer multiple to replace with Bizz: ");
+    scanf("%d", &y);
+    printf("\nEnter integer to count till: ");
+    scanf("%d", &z);
+    
+    int i = 1;
+    while(i<=z)
+    {
+        if (i%x==0 && i%y==0)
+            printf("FizzBizz\n");
+        else if (i%x==0)
+            printf("Fizz\n");
+        else if (i%y==0)
+            printf("Bizz\n");
+        else
+            printf("%d\n", i);
+        i++;
+    }
     return 0;
 }
